9cb453ee23974efbe82983983dc48b1183204db7
Siya021 <siyamthandamazokwana@gmail.com>
2023-05-17 09:42:41 +0200

    First Run

---
+++ script/code.js
@@ -0,0 +1,11 @@
+// let isApproved = true;
+// console.log(typeof isApproved)
+
+let name = "Siyamthanda"
+    lastName = "Mazokwana"
+    age = 24
+    hasGirlfriend = true
+console.log(typeof hasGirlfriend)
+console.log(typeof lastName)
+console.log(typeof age)
+console.log(typeof hasGirlfriend)
